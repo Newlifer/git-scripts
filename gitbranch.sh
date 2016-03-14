a277cf3dcd820061b2dcf49564e32f4bbf5bcf54
aa.novozhilov <aa.novozhilov@tensor.ru>
2016-03-14 10:12:02 +0300

    Add some help.

--- gitbranch.sh
+++ gitbranch.sh
@@ -1,3 +1,12 @@
+argc=$#
+if [ $argc -eq 0 -o $argc -eq 1 ]
+then
+   echo "Some arguments are not specified!"
+   echo "You should explicitly pass by arguments in this way:"
+   echo "gitbranch [feature|bugfix] <branch name>"
+   exit 1
+fi
+
 brnch=$(git status | head -1 | sed 's/On branch //g');
 echo "Current branch: $brnch";
 
@@ -12,4 +21,5 @@ fi
 
 echo "Root branch: $rootname";
 
-git checkout -b $rootname/$1/$2
+git checkout -b $rootname/$1/$2;
+
